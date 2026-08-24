3ae49958b969467a2d1a7fcbe1b6298eebf54296
DevOpsBenjamin <devops.benjamin@gmail.com>
2026-08-24 20:50:34 +0200

    test(playwright): ignore vitest unit spec files in playwright runner

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   testMatch: ['**/*.spec.js'],
+  testIgnore: ['**/*.unit.spec.js'],
   use: {
     baseURL: 'http://localhost:4173',
   },
